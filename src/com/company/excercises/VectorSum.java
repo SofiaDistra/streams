b8c438de46e0505b85c041f9db892de971eb3a74
Sofia Ninou <>
2022-06-17 10:11:52 +0300

    Counting time in vector sum

--- src/com/company/excercises/VectorSum.java
+++ src/com/company/excercises/VectorSum.java
@@ -1,24 +1,44 @@
 package com.company.excercises;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
 public class VectorSum {
 
     public static void main(String args[]) {
-        Random random = new Random();
+
+        IntStream intStream2 = IntStream.rangeClosed(1,100000);
+        // summing sequential
+        long start2 = System.currentTimeMillis();
+        int sum = intStream2.sum();
+        long end2 = System.currentTimeMillis();
 
         // create a stream of random integers
-        IntStream intStream = IntStream.rangeClosed(1,1000);
+        IntStream intStream = IntStream.rangeClosed(1,100000);
         // summing in parallel
+        long start = System.currentTimeMillis();
         int parSum = intStream.parallel().sum();
+        long end = System.currentTimeMillis();
 
-        IntStream intStream2 = IntStream.rangeClosed(1,1000);
-        // summing sequential
-        int sum = intStream2.sum();
+        int[] ints = {7,8,3,5,9,12,45,34,67};
 
-        System.out.println("Parallel sum=" + parSum);
-        System.out.println("Sum=" + sum);
+        // Vector parallel sum using sum()
+        int sum1 = Arrays.stream(ints).parallel().sum();
+
+        // Vector parallel sum using reduce and lambda
+        int sum2 = intStream.parallel().reduce((i,j) -> i+j).getAsInt();
+
+        // Vector parallel sum using reduce and method reference
+        int sum3 = Arrays.stream(ints).parallel().reduce(Integer::sum).getAsInt();
+
+
+
+        long total1 = end-start;
+        System.out.println("Parallel sum=" + parSum + ", ready in " + total1 + " ms");
+        long total2 = end2 - start2;
+        System.out.println("Sum=" + sum + ", ready in " + total2 + " ms");
+
     }
 
 
